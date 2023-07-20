0a922a1d20f2aaa71e851cd2742a0d212cb47d68
Ricardo Najar <ricardo.njar@gmail.com>
2023-07-20 07:48:38 -0700

    cambios

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import  express  from "express"
+import 'dotenv/config'
 
 const app = express()
-app.listen(3000)
-console.log('server on port', 3000)
+app.listen(process.env.PORT)
+console.log('server on port', process.env.PORT)
